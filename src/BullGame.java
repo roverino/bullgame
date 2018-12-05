6fe41edb2387cc9447cec7b7703a9bf105bbc331
roverino <dragoknitee@gmail.com>
2018-12-04 20:44:43 -0500

    added comments

--- src/BullGame.java
+++ src/BullGame.java
@@ -11,14 +11,14 @@ public class BullGame extends JPanel {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Bull Game");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //make x do thing
 
-		final BullGame panel = new BullGame();
-		frame.getContentPane().add(panel);
+		final BullGame panel = new BullGame(); //create panel
+		frame.getContentPane().add(panel); //add panel to the frame
 
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		frame.pack(); //prepare the frame
+		frame.setLocationRelativeTo(null); //center in screen
+		frame.setVisible(true); //see it
 
 		Timer timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
